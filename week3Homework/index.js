39d6c3b026a2211977077eeea388661ca69e0479
samuel <shopifyworld28@gmail.com>
2022-04-25 17:27:40 -0700

    week3Homework

---
+++ week3Homework/index.js
@@ -0,0 +1,38 @@
+const pizzaToppings = ["peperoni", "bacon", "sausage"];
+   
+   function preparePizza([size, crust, list of toppings]) {
+    console.log( "...Cooking pizza...");
+   }
+   return "pizza Object";
+   preparePizza(); 
+
+
+   function greetCustomer(pizzaToppings) {
+    for (let typeof of pizzaToppings)
+    console.log( "Welcome to Pizza House, our toppings are: a, b, c, ...");
+   }
+
+   greetCustomer();
+
+
+
+   function servePizza (pizzaObject) {
+    console.log( "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!");
+   }
+   return "pizza Object";
+   servePizza();
+   
+   function averageIs(solution){
+       console.log();
+   }
+let the Answer = averageOf({1, 2, 3})
+averageIs(averageOf9[1,2,3]));
+
+function getPizzaOrder(size, crust, toppings) {
+       console.log(  "One large thick crust pizza with x, y, z, ... coming up!");
+      }
+      return "size, crust, and toppings";
+      getPizzaOrder();
+
+
+     
